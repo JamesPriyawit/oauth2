09cbbb5e7e96240174cbf5b756f995729c235277
priyawit <priyawit@cupcodeteam.com>
2021-06-18 18:25:30 +0700

    fix

--- src/main/java/com/example/oauth2/controllers/AuthenticationController.java
+++ src/main/java/com/example/oauth2/controllers/AuthenticationController.java
@@ -3,7 +3,7 @@ package com.example.oauth2.controllers;
 import com.example.oauth2.services.AuthenticationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 import javax.servlet.http.HttpServletRequest;
@@ -12,15 +12,11 @@ public class AuthenticationController {
     @Autowired
     private AuthenticationService authenticationService;
 
-    @RequestMapping("/oauth/revoke-token")
+    @PostMapping("/oauth/revoke-token")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void logout(
             HttpServletRequest request
     ){
         authenticationService.logout(request);
     }
-
-    @RequestMapping("/oauth/token")
-    @ResponseStatus(HttpStatus.OK)
-    public
 }
